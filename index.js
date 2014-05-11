6fb1bd20e521b67707794fefa90848ce35234c9f
Dominic Tarr <dominic.tarr@gmail.com>
2014-05-11 21:10:24 +0200

    refactor out pull-delayed-sink

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var defer = require('pull-stream').defer
 var pull = require('pull-stream')
+var lazy = require('pull-delayed-sink')
 
 function once (fun, read) {
   var ended = false
@@ -29,24 +30,6 @@ function header (onHeader, rest) {
 
 function wrap(stream, name) {
   return stream
-}
-
-function lazy (stream) {
-  var read, started = false, id = Math.random()
-
-  function consume (_read) {
-    if(!_read) throw new Error('must be passed a readable')
-    read = _read
-    if(started) stream(read)
-  }
-
-  consume.ready =
-  consume.start = function (_stream) {
-    started = true; stream = _stream || stream
-    if(read) stream(read)
-  }
-
-  return consume
 }
 
 exports = module.exports = function (myHeader, createStream) {
